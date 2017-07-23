e0c3d1aea17ebbdd566634a8097f3eb9bf5d4045
CCInCharge <charles.l.chen.clc@gmail.com>
2017-07-23 15:06:29 -0700

    Add mypython.py

---
+++ py/mypython.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+string1 = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
+string2 = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
+string3 = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
+
+with open('file1.txt', 'w') as file1:
+    file1.write(string1 + '\n')
+
+with open('file2.txt', 'w') as file2:
+    file2.write(string2 + '\n')
+
+with open('file3.txt', 'w') as file3:
+    file3.write(string3 + '\n')
+
+int1 = random.randint(1, 42)
+int2 = random.randint(1, 42)
+
+print(string1)
+print(string2)
+print(string3)
+print(int1)
+print(int2)
+print(int1 * int2)
