1a082a1eeb2bf0bbb88979f61e382840e6e6bb93
CCInCharge <charles.l.chen.clc@gmail.com>
2017-07-29 16:10:02 -0700

    Add comments

--- py/mypython.py
+++ py/mypython.py
@@ -1,10 +1,12 @@
 import random
 import string
 
+# Uses a list comprehension as a shortcut to generate strings of 10 chars
 string1 = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
 string2 = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
 string3 = ''.join(random.choice(string.ascii_lowercase) for x in range(10))
 
+# Creates files and writes strings to them
 with open('file1.txt', 'w') as file1:
     file1.write(string1 + '\n')
 
@@ -14,9 +16,11 @@ with open('file2.txt', 'w') as file2:
 with open('file3.txt', 'w') as file3:
     file3.write(string3 + '\n')
 
+# Generates random ints
 int1 = random.randint(1, 42)
 int2 = random.randint(1, 42)
 
+# Prints all required statements
 print(string1)
 print(string2)
 print(string3)
